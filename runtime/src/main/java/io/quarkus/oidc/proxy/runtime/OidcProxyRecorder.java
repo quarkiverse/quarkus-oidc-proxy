623dd0e0afd42eba43b0ce9d4721be6514d22418
Sergey Beryozkin <sberyozkin@gmail.com>
2026-09-09 17:55:56 +0100

    Revert "skip when tenant is disabled at runtime"

--- runtime/src/main/java/io/quarkus/oidc/proxy/runtime/OidcProxyRecorder.java
+++ runtime/src/main/java/io/quarkus/oidc/proxy/runtime/OidcProxyRecorder.java
@@ -1,18 +1,13 @@
 package io.quarkus.oidc.proxy.runtime;
 
-import org.jboss.logging.Logger;
-
 import io.quarkus.arc.runtime.BeanContainer;
 import io.quarkus.oidc.runtime.TenantConfigBean;
-import io.quarkus.oidc.runtime.TenantConfigContext;
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
 import io.vertx.ext.web.Router;
 
 @Recorder
 public class OidcProxyRecorder {
-
-    private static final Logger LOG = Logger.getLogger(OidcProxyRecorder.class);
 
     final RuntimeValue<OidcProxyConfig> oidcProxyConfig;
 
@@ -22,16 +17,8 @@ public class OidcProxyRecorder {
 
     public void setupRoutes(BeanContainer beanContainer, RuntimeValue<Router> routerValue, String httpRootPath) {
         TenantConfigBean oidcTenantBean = beanContainer.beanInstance(TenantConfigBean.class);
-        TenantConfigContext tenantConfigContext = oidcProxyConfig.getValue().tenantId().isEmpty()
-                ? oidcTenantBean.getDefaultTenant()
-                : oidcTenantBean.getStaticTenantsConfig().get(oidcProxyConfig.getValue().tenantId().get());
-        if (tenantConfigContext.getOidcTenantConfig().tenantEnabled()) {
-            OidcProxy proxy = new OidcProxy(oidcTenantBean, oidcProxyConfig.getValue(), httpRootPath);
-            Router router = routerValue.getValue();
-            proxy.setup(router);
-        } else {
-            LOG.debugf("Skipping OIDC proxy for the disabled tenant '%s'",
-                    oidcTenantBean.getDefaultTenant().getOidcTenantConfig().clientName().orElse("default"));
-        }
+        OidcProxy proxy = new OidcProxy(oidcTenantBean, oidcProxyConfig.getValue(), httpRootPath);
+        Router router = routerValue.getValue();
+        proxy.setup(router);
     }
 }
